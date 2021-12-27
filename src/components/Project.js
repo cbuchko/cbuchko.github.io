2ee90cc85a0f9cf53aa2f5f8c9e4ef93ce25d836
Connor Buchko <connorbuchko2010@yahoo.ca>
2021-12-26 21:55:48 -0800

    youtube works on mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,13 +11,36 @@ const getImage = (imgType) => {
   return "github.png";
 };
 
+const getLink = (mobile, link, imgType) => {
+  if(!mobile){
+    return link;
+  }
+  if(imgType === "youtube"){
+    return link;
+  }
+  return;
+}
+
+const showImage = (link, mobile, imgType) => {
+  if(mobile){
+    if(imgType === "youtube"){
+      return true;
+    }
+    return false;
+  } else {
+    if(link){
+      return true;
+    }
+    return false;
+  }
+}
 export default function Project({ title, img, gif, body, link, imgType }) {
   const mobile = useWindowSize();
   return (
     <div className="br2 project-container">
       <a
         className="no-underline disabled"
-        href={!mobile ? link : undefined}
+        href={getLink(mobile, link, imgType)}
         target="_blank"
         rel="noreferrer"
       >
@@ -25,7 +48,7 @@ export default function Project({ title, img, gif, body, link, imgType }) {
         <div class="project-info pa4 flex h-100 w-100">
           <div className="project-spacing h-100">
             <div className="f5 lh-title">{body}</div>
-            {link && !mobile && (
+            {showImage(link, mobile, imgType) && (
               <img
                 className="project-link mv3"
                 src={getImage(imgType)}
